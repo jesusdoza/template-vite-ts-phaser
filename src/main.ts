4376cebbf5b3ddebfa7c3ebcae21702822bebf42
jesus doza <jesus.doza@gmail.com>
2024-05-03 11:04:49 -0500

    apply physics to single item

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,35 @@
-import { Boot } from './scenes/Boot';
-import { Game as MainGame } from './scenes/Game';
-import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
-import { Preloader } from './scenes/Preloader';
+// import { Boot } from "./scenes/NOT USED/Boot";
+// import { Game as MainGame } from "./scenes/NOT USED/Game";
+// import { GameOver } from "./scenes/NOT USED/GameOver";
+// import { MainMenu } from "./scenes/NOT USED/MainMenu";
+// import { Preloader } from "./scenes/NOT USED/Preloader";
 
 import { Game, Types } from "phaser";
+import { Flappy } from "./scenes/Flappy";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
-const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+
+export const config: Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  // width: window.innerWidth,
+  height: 600,
+  physics: {
+    //arcade physics plugin manages physics simultations
+    default: "arcade",
+  },
+  parent: "game-container",
+  backgroundColor: "#028af8",
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+
+  // scene: [Boot, Preloader, MainMenu, MainGame, GameOver],//defining multiple scens each with its own preload, create, update methods
+
+  //single scene defined and using the functions below
+  scene: [Flappy],
 };
 
 export default new Game(config);
---
+++ src/scenes/Flappy.ts
@@ -0,0 +1,49 @@
+import { Scene } from "phaser";
+import { config } from "../main";
+
+export class Flappy extends Scene {
+  public bird: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
+  constructor() {
+    super("Flappy");
+    //load assets
+  }
+
+  preload() {
+    this.load.setPath("assets");
+
+    //load with key, filename
+    this.load.image("sky", "sky.png");
+    this.load.image("bird", "bird.png");
+  }
+
+  //intialize game instances
+  create() {
+    const width = config?.width ? Number(config.width) : 500;
+    const height = config?.height ? Number(config.height) : 500;
+
+    //add image at position x ,y and reference from above
+    this.add.image(width / 2, height / 2, "sky");
+
+    //bird with just display and no physics
+    // const bird = this.add.sprite(width / 10, height / 2, "bird").setOrigin(0);
+
+    //add bird with physics body so it will have a body
+    this.bird = this.physics.add
+      .sprite(width / 10, height / 2, "bird")
+      .setOrigin(0);
+    console.log("bird.body", this.bird.body);
+
+    // bird.body.gravity.y = 200; //builds up to 200 px per second
+    this.bird.body.velocity.y = 200; //velocity starts at 200 and stays there
+  }
+
+  //update called every frame
+  //normally 60fps so 60 times per second called
+  //time is currenty time
+  //delta time since last frame
+  update(time, delta) {
+    //gravity starts at velocity 0 then build up to gravitry
+    console.log("this.bird.body.gravity.y", this.bird.body.gravity.y);
+  }
+}
