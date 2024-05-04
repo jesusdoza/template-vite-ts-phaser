8bd26d5dcc3300c79a590dab202dda4930c2fe6a
jesus doza <jesus.doza@gmail.com>
2024-05-03 19:05:42 -0500

    change direction of velocity in update method of class and take into account item body with

--- src/scenes/Flappy.ts
+++ src/scenes/Flappy.ts
@@ -3,6 +3,9 @@ import { config } from "../main";
 
 export class Flappy extends Scene {
   public bird: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  VELOCITY = 200;
+  width = config?.width ? Number(config.width) : 500;
+  height = config?.height ? Number(config.height) : 500;
 
   constructor() {
     super("Flappy");
@@ -19,20 +22,27 @@ export class Flappy extends Scene {
 
   //intialize game instances
   create() {
-    const width = config?.width ? Number(config.width) : 500;
-    const height = config?.height ? Number(config.height) : 500;
-
     //add image at position x ,y and reference from above
-    this.add.image(width / 2, height / 2, "sky");
+    this.add.image(this.width / 2, this.height / 2, "sky");
 
     //add bird with physics body so it will have a body
     this.bird = this.physics.add
-      .sprite(width / 10, height / 2, "bird")
-      .setOrigin(0);
-    console.log("bird.body", this.bird.body);
+      .sprite(this.width / 10, this.height / 2, "bird")
+      .setOrigin(0, 0);
 
-    // this.bird.body.velocity.y = 200; //velocity starts at 200 and stays there?
+    this.bird.body.velocity.x = this.VELOCITY; //velocity starts at 200 and stays there?
+
+    console.log("bird.body.velocity", this.bird.body);
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    if (this.bird.x >= this.width - this.bird.width) {
+      this.bird.body.velocity.x = -this.VELOCITY;
+      console.log("this.bird.body.position", this.bird.body.position);
+    } else if (this.bird.x <= 0) {
+      this.bird.body.velocity.x = this.VELOCITY;
+      console.log("this.bird.body.position", this.bird.body.position);
+      return;
+    }
+  }
 }
